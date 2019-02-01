4a73f49ba6a0af28f42f06768a7427068bd9d575
Ben Rapp <ben.rapp22@gmail.com>
2019-02-01 17:31:32 -0500

    Edit 2 (2/1/2019)

--- Battleship.py
+++ Battleship.py
@@ -1,23 +1,6 @@
 import numpy as np
 
-#Horizontal Grid
-ah = np.arange(1, 11, 1)
-bh = np.arange(11, 21, 1)
-ch = np.arange(21, 31, 1)
-dh = np.arange(31, 41, 1)
-eh = np.arange(41, 51, 1)
-fh = np.arange(51, 61, 1)
-gh = np.arange(61, 71, 1)
-hh = np.arange(71, 81, 1)
-ih = np.arange(81, 91, 1)
-jh = np.arange(91, 101, 1)
 
-z = np.array([[ah], [bh], [ch], [dh], [eh], [fh], [gh], [hh], [ih], [jh]])
-
-def boardmodel():
-    print(z)
-
-horizontalgrid = [ah, bh, ch, dh, eh, fh, gh, hh, ih, jh]
 #Introduction
 print("Welcome to Battleship! This is a 1-player game, in which the objective is to sink your opponent's battleships!")
 print("Please place your own ships according to the diagram below, and only place your ships horizontally or vertically")
@@ -29,39 +12,79 @@ print("2x Destroyer (2 Spaces Long)")
 print("2x Submarine (1 Space Long)")
 print("When entering your ")
 print("Choose your positions wisely!")
-boardmodel()
 
-#User Places Ships
-carrier1 = input("Aircraft Carrier Space 1: ")
-carrier2 = input("Aircraft Carrier Space 2: ")
-carrier3 = input("Aircraft Carrier Space 3: ")
-carrier4 = input("Aircraft Carrier Space 4: ")
-carrier5 = input("Aircraft Carrier Space 5: ")
+#Displaying Game Board
+a = np.arange(1, 11, 1)
+list1 = [a]
+array1 = np.array(list1, dtype = 'int')
 
-battleship1 = input("Battleship Space 1: ")
-battleship2 = input("Battleship Space 2: ")
-battleship3 = input("Battleship Space 3: ")
-battleship4 = input("Battleship Space 4: ")
+for i in range(0,10):
+    print(array1 + (10*i))
 
-cruiser1 = input("Cruiser Space 1: ")
-cruiser2 = input("Cruiser Space 2: ")
-cruiser3 = input("Cruiser Space 3: ")
+print("Use the following characters to select which type of ship you would like to place: ")
+print("Aircraft Carrier -- AC")
+print("Battleship -- B")
+print("Cruiser -- C")
+print("Destroyer -- D")
+print("Submarine -- S")
 
-destroyerA1 = input("Destroyer A Space 1: ")
-destroyerA2 = input("Destroyer B Space 2: ")
+#Placing Ships
+x = 0
+howmanyships = 0
+shiplength = 0
 
-destroyerB1 = input("Destroyer B Space 1: ")
-destroyerB2 = input("Destroyer B Space 2: ")
+while 1:
+    while 1:
+        whichship = input("Which ship would you like to place? ")
+        if whichship == "AC":
+            shiplength = 5
+            howmanyships = howmanyships + 1
+            break
+        elif whichship == "B":
+            shiplength = 4
+            howmanyships = howmanyships + 1
+            break
+        elif whichship == "C":
+            shiplength = 3
+            howmanyships = howmanyships + 1
+            break
+        elif whichship == "D":
+            shiplength == 2
+            howmanyships = howmanyships + 1
+            break
+        elif whichship == "S":
+            shiplength = 1
+            howmanyships = howmanyships + 1
+            break
+        else:
+            print("Try Again!")
+    while 1:
+        try:
+            startinput = int(input("Please Select Starting Space (keep in mind that ships are placed from left --> right and up --> down): "))
+            if 1 <= startinput <= 100:
+                break
+            else:
+                print("Choose a space on the board!")
+        except:
+            print("Try Again!")
+    Ship1 = [startinput, ]
+    while 1:
+        directioninput = input("Please Select Direction (type RIGHT to place from left --> right, and DOWN to place from up --> down): ")
+        if directioninput == "RIGHT":
+            for i in range(1, int(shiplength)):
+                Ship1.append(startinput + (i + x))
+            break
+        elif directioninput == "DOWN":
+            for i in range(1, int(shiplength)):
+                Ship1.append(startinput + ((10*i) + x))
+            break
+        else:
+            print("Try Again!")
+    print(str(whichship) + ": " + str(Ship1))
+    if howmanyships == 7:
+        break
 
-submarineA = input("Submarine A Position: ")
-submarineB = input("Submarine B Position: ")
 
-if carrier2 == (carrier1 + 1):
-
-
-
-
-#Another array with vertical orietnation
-#User inputs specific numbers to indicate location of Ships
-#Stores as a variable, tests against array(s)
-#Computer attacks with a random number
+#SHIPS CANT OVERLAP
+#AC, B, and C CAN ONLY BE SELECTED ONCE
+#FORCE USER TO MAKE PROPER INPUTS
